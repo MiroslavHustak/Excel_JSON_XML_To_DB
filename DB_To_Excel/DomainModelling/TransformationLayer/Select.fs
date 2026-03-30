6b5a4e677432f4a7e0eb54d7fadaecb0bb55c52b
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2026-03-31 01:38:37 +0200

    31-03-2026

--- DB_To_Excel/DomainModelling/TransformationLayer/Select.fs
+++ DB_To_Excel/DomainModelling/TransformationLayer/Select.fs
@@ -20,52 +20,55 @@ let internal selectAsync (connection: Async<Result<SqlConnection, string>>) tabl
 
                 let query = sprintf "SELECT * FROM %s" tableName
                 use cmd = new SqlCommand(query, connection)
+                                
+                // let! reader = cmd.ExecuteReaderAsync() |> Async.AwaitTask // Pay attention to why it is so (not use! reader = ... no disposal guarantee)
+                // use reader = reader // Pay attention to why it is so 
 
-                let! reader = cmd.ExecuteReaderAsync() |> Async.AwaitTask
-        
-                try
-                    let indexName     = reader.GetOrdinal "Jmeno"
-                    let indexSurname  = reader.GetOrdinal "Prijmeni"
-                    let indexSSN      = reader.GetOrdinal "RC"
-                    let indexBirthDay = reader.GetOrdinal "DatumNarozeni"
+                use! reader = cmd.ExecuteReaderAsync() |> Async.AwaitTask |> Async.map Ok // Pay attention to why it is so 
 
-                    let records =
-                        ()
-                        |> AsyncSeq.unfoldAsync  // Generator is repeatedly called until it returns None.
-                            (fun () 
-                                -> 
-                                async 
-                                    {
-                                        let! successfullyRead = reader.ReadAsync() |> Async.AwaitTask //use! supposedly to behave improperly in the AsyncSeq loop
+                (*
+                cmd.ExecuteReaderAsync()          // Task<SqlDataReader>
+                |> Async.AwaitTask                // Async<SqlDataReader>
+                |> Async.map Ok                   // Async<Result<SqlDataReader, string>>
+                *)
+                                                      
+                let indexName     = reader.GetOrdinal "Jmeno"
+                let indexSurname  = reader.GetOrdinal "Prijmeni"
+                let indexSSN      = reader.GetOrdinal "RC"
+                let indexBirthDay = reader.GetOrdinal "DatumNarozeni"
 
-                                        match successfullyRead with
-                                        | true  
-                                            ->
-                                            //reader.GetString returns "" for empty DB strings, not null
-                                            let record : PersonDbIntoDtm = //DateTime is a struct, so it cannot be null. 
-                                                {
-                                                    Jmeno         = reader.GetString indexName    |> Option.ofNullEmptySpace
-                                                    Prijmeni      = reader.GetString indexSurname |> Option.ofNullEmptySpace
-                                                    RC            = reader.GetString indexSSN     |> Option.ofNullEmptySpace
-                                                    DatumNarozeni = match reader.IsDBNull indexBirthDay with true -> None | false -> reader.GetDateTime indexBirthDay |> Some
-                                                }
+                let records =
+                    ()
+                    |> AsyncSeq.unfoldAsync  // Generator is repeatedly called until it returns None.
+                        (fun () 
+                            -> 
+                            async 
+                                {
+                                    let! successfullyRead = reader.ReadAsync() |> Async.AwaitTask //use! supposedly to behave improperly in the AsyncSeq loop
 
-                                            return Some (record, ())  // (value, next_state)
+                                    match successfullyRead with
+                                    | true  
+                                        ->
+                                        //reader.GetString returns "" for empty DB strings, not null
+                                        let record : PersonDbIntoDtm = //DateTime is a struct, so it cannot be null. 
+                                            {
+                                                Jmeno         = reader.GetString indexName    |> Option.ofNullEmptySpace
+                                                Prijmeni      = reader.GetString indexSurname |> Option.ofNullEmptySpace
+                                                RC            = reader.GetString indexSSN     |> Option.ofNullEmptySpace
+                                                DatumNarozeni = match reader.IsDBNull indexBirthDay with true -> None | false -> reader.GetDateTime indexBirthDay |> Some
+                                            }
 
-                                        | false 
-                                            ->
-                                            return None
-                                    }
-                            )
+                                        return Some (record, ())  // (value, next_state)
 
-                    let! results = records |> AsyncSeq.toListAsync  // Accumulate results asynchronously
+                                    | false 
+                                        ->
+                                        return None
+                                }
+                        )
 
-                    return! Ok results
+                let! results = records |> AsyncSeq.toListAsync  // Accumulate results asynchronously
 
-                finally
-                    reader.DisposeAsync().AsTask()
-                    |> Async.AwaitTask
-                    |> Async.StartImmediate
+                return! Ok results       
                     
             with
             | ex -> return! Error <| string ex.Message
