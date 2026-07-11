5034bffa26e2e686f89a648f744088daa4031139
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2026-07-11 21:39:27 +0200

    11-07-2026

--- Excel_JSON_XML_To_DB/Program.fs
+++ Excel_JSON_XML_To_DB/Program.fs
@@ -28,93 +28,89 @@ open TransformationLayerDbToExcel
 
 //*****************************
 
-[<EntryPoint>] 
-let main argv =    
+let private cacheAsync (computation : Async<'T>) : Async<'T> =
+
+    let task = computation |> Async.StartAsTask
+
+    async { return! task |> Async.AwaitTask }
+
+[<EntryPoint>]
+let main argv =
 
     let connectionAsync =
-          async 
-              {
-                  match! getAsyncConnection() with
-                  | Error err 
-                      ->
-                      return Error err
-                  | Ok connection 
-                      ->
-                      return! async { return Ok connection }
-              }          
-                         
+        getAsyncConnection ()
+        |> cacheAsync
+
     //let fullPath = @"e:\source\repos\Excel_JSON_XML_To_DB\Excel_JSON_XML_To_DB\Excel\ExcelFile\excelNightwish2013.xlsx"
-    
+
     //*********** Excel************
     (*
     let result1 = readDataFromExcelClosedXML fullPath
     result1 |> printfn "%A"
-    
+
     printfn "*************************************************"
 
-    let result2 = readDataFromExcelTP() 
+    let result2 = readDataFromExcelTP()
     result2 |> printfn "%A"
 
     match result1, result2 with
-    | r1, r2 
+    | r1, r2
         when r1 = r2
-        -> printfn "Results are the same."  
-    | _ -> printfn "Results are different."   
+        -> printfn "Results are the same."
+    | _ -> printfn "Results are different."
     *)
 
     (*
     let result1 = transformedListClosedXML fullPath
     result1 |> printfn "%A"
-    
+
     printfn "*************************************************"
 
-    let result2 = transformedListTP() 
+    let result2 = transformedListTP()
     result2 |> printfn "%A"
 
     match result1, result2 with
-    | r1, r2 
+    | r1, r2
         when r1 = r2
-        -> printfn "Results are the same."  
-    | _ -> printfn "Results are different."   
+        -> printfn "Results are the same."
+    | _ -> printfn "Results are different."
     *)
-    //*****************************       
-   
-    //*********** JSON ************
+    //*****************************
 
-    //let result2 = readDataFromJsonTP() 
+    //*********** JSON ************
+    //let result2 = readDataFromJsonTP()
     //result2 |> printfn "%A"
     //*****************************
 
     //*********** XML ************
-
-    let result3 = readDataFromXmlTP() 
+    let result3 = readDataFromXmlTP ()
     result3 |> printfn "%A"
     //*****************************
-   
+
     //let persons = transformedListClosedXML fullPath //For an Excel file only
-    let persons = transformedListTP() 
-            
+    let persons = transformedListTP ()
+
     match insertOrUpdateAsyncFailFast persons connectionAsync |> Async.RunSynchronously with
     | Ok _      -> printfn "\nInserting or updating successful"
     | Error err -> err |> printfn "\n%s"
 
     //*********** DB to Excel ************
     let result4 =
-        async 
+        async
             {
                 let tableName = "TabA"
                 let data = getAsyncConnection >> transformAsync <| () <| tableName
                 return! writeDataIntoExcelWithFsExcel @"e:\source\repos\Excel_JSON_XML_To_DB\Nightwish2013_FromDb.xlsx" data
             }
         |> Async.RunSynchronously
-    
+
     match result4 with
     | Ok _      -> printfn "\nTransferring data from DB into Excel successful"
-    | Error err -> err |> printfn "\n%s"  
-        
-    //Example of a connection close strategy 
+    | Error err -> err |> printfn "\n%s"
+
+    //Example of a connection close strategy
     match closeAsyncConnection connectionAsync |> Async.RunSynchronously with
     | Ok _      -> printfn "\nConnection closed successfully."
-    | Error err -> printfn "\nConnection closing failure: %s." err 
-    
-    0   
+    | Error err -> printfn "\nConnection closing failure: %s." err
+
+    0
